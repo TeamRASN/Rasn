681c7c06320e35bb6b8dbfc775728e7a1a2e897b
Agustin Rezett <nazareno.agustin45@gmail.com>
2021-08-07 11:05:59 -0300

    FIX - Overflow del header y ajustes de breakpoints en el carousel

--- src/components/Inicio/Equipo.js
+++ src/components/Inicio/Equipo.js
@@ -8,12 +8,72 @@ import Integrante from "./tarjetas/Integrante";
 
 //Objetos
 const breakPoints = [
-	{ width: 1, itemsToShow: 1 },
-	{ width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
-	{ width: 850, itemsToShow: 3 },
-	{ width: 1150, itemsToShow: 4, itemsToScroll: 2 },
+	{ width: 1, itemsToShow: 1, itemPadding: [0, 20], pagination: false },
+	{ width: 400, itemsToShow: 2, itemsToScroll: 2 },
+	{ width: 650, itemsToShow: 3 },
+	{ width: 1150, itemsToShow: 4 },
 	{ width: 1450, itemsToShow: 5 },
 	{ width: 1750, itemsToShow: 6 },
+];
+
+//Objetos
+const memberCards = [
+	{
+		id: 1,
+		name: "Micaela",
+		surname: "Darrel",
+		image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+		rol: "Voluntaria",
+	},
+	{
+		id: 2,
+		name: "Hernan",
+		surname: "Vargas",
+		image: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+		rol: "Voluntario",
+	},
+	{
+		id: 3,
+		name: "Paola",
+		surname: "Thomson",
+		image: "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+		rol: "Organizadora",
+	},
+	{
+		id: 4,
+		name: "Miguel",
+		surname: "Fernandez",
+		image: "https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+		rol: "Voluntario",
+	},
+	{
+		id: 5,
+		name: "Carolina",
+		surname: "Gomez",
+		image: "https://images.unsplash.com/photo-1554384645-13eab165c24b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80",
+		rol: "Organizadora",
+	},
+	{
+		id: 6,
+		name: "Lucas",
+		surname: "Essandy",
+		image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+		rol: "Voluntario",
+	},
+	{
+		id: 7,
+		name: "Uriel",
+		surname: "Franco",
+		image: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=335&q=80",
+		rol: "Voluntario",
+	},
+	{
+		id: 8,
+		name: "Tomas",
+		surname: "Birgo",
+		image: "https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+		rol: "Voluntario",
+	},
 ];
 
 export default function Equipo() {
@@ -34,11 +94,9 @@ export default function Equipo() {
 					itemPadding={[10, 20]}
 					breakPoints={breakPoints}
 				>
-					<Integrante />
-					<Integrante />
-					<Integrante />
-					<Integrante />
-					<Integrante />
+					{memberCards.map(({ id, name, surname, image, rol }) => (
+						<Integrante name={name} surname={surname} image={image} rol={rol} key={id} />
+					))}
 				</Carousel>
 			</div>
 		</section>
--- src/components/Inicio/Tarjetas/Integrante.js
+++ src/components/Inicio/Tarjetas/Integrante.js
@@ -1,18 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //Componentes
 import Persona from "../../../assets/Cat.jpg";
 
-export default function Integrante() {
+export default function Integrante({ name, surname, image, rol }) {
 	return (
 		<div className="item integrante">
 			<div className="testimony-wrap text-center">
-				<div className="user-img" style={{ backgroundImage: `url(${Persona})` }}></div>
+				<div className="user-img" style={{ backgroundImage: `url(${image})` }}></div>
 				<div className="text">
-					<p className="name">Nombre</p>
-					<span className="position">Voluntario</span>
+					<p className="name">
+						{surname}, {name}
+					</p>
+					<span className="position">{rol}</span>
 				</div>
 			</div>
 		</div>
 	);
 }
+
+Integrante.propTypes = {
+	name: PropTypes.string,
+	surname: PropTypes.string,
+	image: PropTypes.string,
+	rol: PropTypes.string,
+};
